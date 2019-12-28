f7f13e54bcbe153135735e82ae4d3100808ee830
Simon Grozovsky <svgroz@yandex.ru>
2019-12-29 01:34:01 +0300

    init

--- src/rnet.cpp
+++ src/rnet.cpp
@@ -1,12 +1,36 @@
-#include <iostream>
-#include "asio.hpp"
 #include "rnet.h"
+#include <asio.hpp>
+#include <iostream>
+#include <memory>
 
-rserver::rserver() {
-	
+using asio::ip::tcp;
+
+RServer::RServer(unsigned short port,
+                 const std::function<void()> doOnConnection,
+                 const std::function<void(std::error_code)> doOnError)
+    : context(),
+      tcpAcceptor(context, tcp::endpoint(tcp::v4(), port)),
+      doOnConnection(doOnConnection),
+      doOnError(doOnError) {
+  std::cout << "rserver created" << std::endl;
+}
+
+RServer::~RServer() { std::cout << "rserver deleted" << std::endl; }
+
+void RServer::DoOnConnection() const {
+  if (this->doOnConnection) {
+    this->doOnConnection();
+  }
+}
+
+void RServer::DoOnError(const std::error_code errorCode) const {
+  if (this->doOnError) {
+    this->doOnError(errorCode);
+  }
 }
 
 int main() {
-	asio::io_context context;
-	return 0;
+  RServer(
+      8080, []() {}, [](const std::error_code) {});
+  return 0;
 }
--- src/rnet.h
+++ src/rnet.h
@@ -1,14 +1,23 @@
 #ifndef RNET_H
 #define RNET_H
 
-#include <asio.hpp>
+#include <functional>
+#include <memory>
+#include "asio.hpp"
 
-class rserver {
-	public:
-			rserver();
-	private:
-			const asio::io_context context;
+class RServer {
+ public:
+  RServer(unsigned short port, const std::function<void()> doOnConnection,
+          const std::function<void(std::error_code)> doOnError);
+  virtual ~RServer();
+
+ private:
+  asio::io_context context;
+  asio::ip::tcp::acceptor tcpAcceptor;
+  const std::function<void()> doOnConnection;
+  const std::function<void(std::error_code)> doOnError;
+  virtual void DoOnConnection() const;
+  virtual void DoOnError(const std::error_code error_code) const;
 };
- 
-#endif
 
+#endif
